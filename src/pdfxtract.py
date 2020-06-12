6824949bdb83b1dce868f5e83148bace4b7d253e
AvedisBaghdasarian <ab6951@nyu.edu>
2020-06-12 01:19:01 -0400

    updated pdf_to_txt to use files stored on s3

--- src/pdfxtract.py
+++ src/pdfxtract.py
@@ -1,4 +1,3 @@
-
 """
 adapted from https://gist.github.com/jmcarp/7105045#file-pdfxtract-py
 """
@@ -6,16 +5,21 @@ adapted from https://gist.github.com/jmcarp/7105045#file-pdfxtract-py
 Extract PDF text using PDFMiner. Adapted from
 http://stackoverflow.com/questions/5725278/python-help-using-pdfminer-as-a-library
 """
-import time
+
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter#process_pdf
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+import boto3
+import tempfile
 
 from io import StringIO
 
+import json
+
+
 def pdf_to_text(pdfnames):
-    t = time.time()
+
     text = []
     # PDFMiner boilerplate
     rsrcmgr = PDFResourceManager()
@@ -26,22 +30,34 @@ def pdf_to_text(pdfnames):
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     dictionary = [["ﬀ" , "ff"],["ﬁ","fi"],["ﬂ", "fl"],['ﬃ', 'ffi'],['ﬄ', 'ffl'],['ﬅ', 'ft'],['ﬆ', 'st'], ['"', ''],["'",'' ] ]
 
+
+
     for pdfname in pdfnames:
-        # Extract text
-        print(pdfname)
-        fp = open(pdfname, 'rb')
-        for page in PDFPage.get_pages(fp):
-            interpreter.process_page(page)
-        fp.close()
 
 
-        # Get text from StringIO
-        newtext = sio.getvalue()
-        for word in dictionary:
-            newtext = newtext.replace(word[0], word[1])
+        s3 = boto3.resource('s3')
+        with tempfile.TemporaryFile() as f:
+            s3.meta.client.download_fileobj('testpdflake', '1001/1001.0008.pdf', f)
+            f.seek(0)
+            print('hi', f)
 
-        text.append(newtext)
-        print(time.time()-t)
+
+            # Extract text
+            fp = f
+            for page in PDFPage.get_pages(fp):
+                print('hi')
+                interpreter.process_page(page)
+            fp.close()
+
+
+            # Get text from StringIO
+            newtext = sio.getvalue()
+            for word in dictionary:
+                newtext = newtext.replace(word[0], word[1])
+
+            text.append(newtext)
+        print("hi" ,text)
+
 
     # Cleanup
     device.close()
